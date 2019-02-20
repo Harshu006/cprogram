da62a0cd92c5b7553d824e1b738c0a48a8b4fd06
vaibhavkalia <47554468+vaibhavkalia@users.noreply.github.com>
2019-02-20 10:56:26 +0530

    Add files via upload
    The classic hello world program.

---
+++ 01HELLO.C
@@ -0,0 +1,9 @@
+/*Classic Hello World Program */
+
+/*The program has only one output statement (printf() */
+/*To enable exection of this statement, it needs to be wrapped into main() */
+
+main()
+{
+	printf("Hello World!");
+}
