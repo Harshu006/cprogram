24e1a66c3a0ec3bf59517136c549c350b4fbb317
Vaibhav Kalia <47554468+vaibhavkalia@users.noreply.github.com>
2019-04-08 11:06:10 +0530

    Update 38StringFunctions.c
    code comments and explanation

--- 38StringFunctions.c
+++ 38StringFunctions.c
@@ -61,6 +61,12 @@ main()
 	getch();
 
 }
+
+/*String Length is number of characters in string (excluding '\0', as it is an implementation overhead.
+Working: We take the starting address of string (address of first character of string -> as a char pointer)
+then start reading each character via pointer and see if is not '\0' -> move to next character
+--> simultaenously count also in i, how many steps you have moved forward before you encountered '\0'
+*/
 int mystrlen(char *s)
 {
 	int i=0;
@@ -70,6 +76,27 @@ int mystrlen(char *s)
 	}
 	return i;
 }
+
+/*string concatenation is a process in which second string is added at the end of first string (combining the strings)
+Working: find out length of strings (destination=>j and source=>length)
+take the character of source and place it after j'th location in destination.
+-------------------------------------------------------
+destination               source
+01234567890(j)            01234 (length)
+hello0                    hi0   <- read 0 as '\0'
+-------------------------------------------------------
+from src pick 'h' at 0 index and place it at index 5 of dest
+from src pick 'i' at 1 index and place it at index 5+1 of dest
+repeat this process length times  (i.e. for each element of source)
+resultant string is
+-------------------------------------------------------
+destination
+0123456789
+hellohi
+-------------------------------------------------------
+
+Because we have overwritten '\0' of destination, we must add it manually 
+*/
 
 void mystrcat(char* dest, char* src)
 {
@@ -85,6 +112,10 @@ void mystrcat(char* dest, char* src)
 	*(dest+j+i)='\0';
 }
 
+/*string copy copies one string to another (overwriting destination)
+Working: Pick source string, start from begining and for each character write it to destination string. 
+stop when '\0' is found in source, write this '\0' in destination manually
+*/
 void mystrcpy(char* dest, char* src)
 {
 	while(*src != '\0')
@@ -95,6 +126,21 @@ void mystrcpy(char* dest, char* src)
 	}
 	*dest = '\0';
 }
+
+/*String Comparison is actually the numeric comparison of ASCII values of each string.
+Output is an integer which is interpreted as:
+NEGATIVE: first string is smaller than second
+ZERO : both strings are equal
+POSITIVE: first string is greater than second
+[smaller / greater is numeric difference of ASCII character]
+
+two scenarios are possible:
+1. when string are identical -> we find each character is equal
+2. when string are different -> we return the numeric difference between 
+   the first non-similar character encountered
+Working: Keep checking characters till a different character is found -> return subtracted value of ASCII values   
+   when  there is no different character and string ends -> return zero
+*/
 int mystrcmp(char* a, char* b)
 {
 	int i=0;
